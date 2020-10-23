ea280346d62e16166aae688ce5dfa059dd30e5d0
Christian Fei <crifei93@gmail.com>
2020-10-23 12:38:16 +0200

    generate tools from tsv

---
+++ generate-tools-from-tsv.js
@@ -0,0 +1,37 @@
+const fsp = require('fs').promises
+const path = require('path')
+
+async function main () {
+  console.log('generate-tools-from-tsv')
+  const tsvContents = await fsp.readFile(path.resolve(__dirname, '_data', 'tools.tsv'), { encoding: 'utf8' })
+  const tsvByLines = tsvContents.split('\n')
+  const header = tsvByLines.splice(0, 1)[0]
+  // console.log(tsvByLines)
+  // console.log(header)
+  const fields = header.split('\t').map(t => t.replace('\r', ''))
+  // console.log(header, fields)
+  const json = tsvByLines
+    .map(tsvLineToObject(fields))
+    .map(addPermalink)
+
+  // console.log(json)
+  await fsp.writeFile(path.resolve(__dirname, '_data', 'tools.json'), JSON.stringify(json, null, 2), { encoding: 'utf8' })
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => console.error(err) && process.exit(1))
+
+function tsvLineToObject (fields) {
+  return line => line
+    .split('\t')
+    .map(l => l.replace('\r', ''))
+    .reduce((acc, curr, index) => ({ ...acc, [fields[index]]: curr }), {})
+}
+
+function addPermalink (tool) {
+  return {
+    ...tool,
+    url: '/' + tool['Nome tool'].toLowerCase().replace(/\s/gi, '-').replace(/\//gi, '-') + '/'
+  }
+}
--- tools/a-b-test.md
+++
@@ -1,7 +0,0 @@
----
-title: A/B Test
-tags: tool
-layout: layouts/tool.njk
----
-
-L'A/B testing è un esperimento controllato che prendendo in considerazione due varianti dello stesso progetto online determina quale delle due versioni sia più performante secondo una metrica di successo misurabile. 
