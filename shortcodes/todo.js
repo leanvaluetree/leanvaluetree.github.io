a835f958a0ffc5d6c12dcc5862a0d2af93e77450
Christian Fei <crifei93@gmail.com>
2021-03-05 15:11:03 +0100

    todo list with arrows

--- shortcodes/todo.js
+++ shortcodes/todo.js
@@ -1,18 +1,14 @@
 module.exports = function todo (tool) {
-  // console.log(tool['To-do'])
-  const bullet = `
-  <svg class="inline mr-4" style="vertical-align: baseline;" width="9" height="17" viewBox="0 0 9 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9 8.92578C4.58172 8.92578 1 5.34406 1 0.925781" stroke="#201D25" stroke-width="2"/>
-    <path d="M9 8.92578C4.58172 8.92578 1 12.5075 1 16.9258" stroke="#201D25" stroke-width="2"/>
-  </svg>
-  `
-
   return `
   <div class="todo">
     <h3>To-do</h3>
-    <p>
-      ${tool['To-do'].replace(/- /gmi, bullet).replace(/\\n/gi, '<br/><br/>')}
-    </p>
+    <ul class="list-with-arrows">
+      ${tool['To-do']
+        .replace(/- /gmi, '')
+        .split(/\\n/)
+        .filter(Boolean)
+        .map(c => `<li>${c}</li>`).join('')}
+    </ul>
   </div>
   `.trim()
 }
